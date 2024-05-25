c479ec591a878ea31d1190f8ec98453fb3a6ad60
agorohov <aleksandr.gorohov@gmail.com>
2024-05-25 09:57:14 +0300

    начал работать с реализацией DepartmentServiceImplTest

--- src/test/java/com/agorohov/employeebookwithmockito/service/DepartmentServiceImplTest.java
+++ src/test/java/com/agorohov/employeebookwithmockito/service/DepartmentServiceImplTest.java
@@ -1,8 +1,19 @@
 package com.agorohov.employeebookwithmockito.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class DepartmentServiceImplTest {
+
+    @Mock
+    private EmployeeService employeeServiceMock;
+
+    @InjectMocks
+    private DepartmentService out;
 
     @Test
     void getSumMonthSalaries() {
